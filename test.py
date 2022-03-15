19b32d636cdf01a7baa6e24085872281ae5f655b
Abdulhai Naqvi <abdulhai.naqvi@windriver.com>
2022-03-15 00:16:06 -0700

    Removed debugging print statements and updated README.md . Original template source can be found in previous commit.

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 
 def test_wrong_date_format():
+    '''Wrong input format'''
     interpreter_location = sys.executable
     csv_file = "cookies.log"
     targetDay = "12/09/2020"
@@ -10,6 +11,7 @@ def test_wrong_date_format():
     assert(output.returncode != 1)
 
 def test_wrong_date_len():
+    '''Wrong input format'''
     interpreter_location = sys.executable
     logFile = "cookies.log"
     targetDay = "12-09-02020"
@@ -17,6 +19,7 @@ def test_wrong_date_len():
     assert(output.returncode != 1)
 
 def test_one_cookie():
+    '''Only one matching cookie'''
     interpreter_location = sys.executable
     logFile = "cookies.log"
     targetDay = "2018-12-09"
@@ -24,10 +27,19 @@ def test_one_cookie():
     assert(output.returncode == 1)
 
 def test_multiple_cookies():
+    '''Multiple matching cookies.'''
     interpreter_location = sys.executable
     logFile = "cookies.log"
     targetDay = "2018-12-07"
     output = subprocess.run([interpreter_location, "most_active_cookie.py", logFile, "-d", targetDay], capture_output=True, encoding='utf-8')
     targetOutput = "4sMM2LxV07bPJzwf\nfbcn5UAVanZf6UtG\n"
-    print(f"----\n {output.stdout} \n------")
+    assert(output.stdout == targetOutput)
+
+def test_no_cookie():
+    '''No matching cookie'''
+    interpreter_location = sys.executable
+    logFile = "cookies.log"
+    targetDay = "2019-12-22"
+    output = subprocess.run([interpreter_location, "most_active_cookie.py", logFile, "-d", targetDay], capture_output=True, encoding='utf-8')
+    targetOutput = ""
     assert(output.stdout == targetOutput)
